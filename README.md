590f237aaf7d94c835337e3528942cf0e201e94e
Ayushi Gajera <ayushigajera017@gmail.com>
2024-01-12 19:18:02 +0530

    Add Razorpay Website Clone

---
+++ README.md
@@ -0,0 +1,3 @@
+# Razorpay Website Clone
+
+A Razorpay clone mirrors the clean and built with HTML and styled with Tailwind CSS, my clone ensures a responsive and adaptive layout, offering an optimal viewing experience across various devices, be it desktop, tablet, or mobile.
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontSize: {
+      xs: '0.5rem',
+      sm: '0.9rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    extend: {fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+    },},
+  },
+  plugins: [],
+}
